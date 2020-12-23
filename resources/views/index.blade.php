f164a6adc5e075e4ecc8d7102c099b7d33f711bf
daigonstar <daigonstar@gmail.com>
2020-12-23 14:42:06 +0000

    Changed spacing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,8 @@
 @section('main_content')
 
     <div>
-            <h1 class="display-4, text-center"><span class="text-info">Welcome</span></h1>
-            <h2 class="display-3, text-center"><span class="text-info">My Game Tracker</span><h2>
+        <h1 class="display-4, text-center"><span class="text-info">Welcome</span></h1>
+        <h2 class="display-3, text-center"><span class="text-info">My Game Tracker</span><h2>
     </div>
 
 @endsection
