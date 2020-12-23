1a80c0b5a6f35516118e3e85ccd64646c0ccd06f
daigonstar <daigonstar@gmail.com>
2020-12-23 14:45:20 +0000

    removed additional spacing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,8 @@
 
 @extends('layout.page')
 @section('main_content')
-
     <div>
         <h1 class="display-4, text-center"><span class="text-info">Welcome</span></h1>
         <h2 class="display-3, text-center"><span class="text-info">My Game Tracker</span><h2>
     </div>
-
 @endsection
